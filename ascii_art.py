59632f0dc6647a857d88717c315cc4dc7fc3f5bf
Gajanan Borade <gajananborade11@gmail.com>
2025-08-15 00:47:15 +0200

    first commit - created ascii_art.py with STAGES and MAX_MISTAKES

---
+++ ascii_art.py
@@ -0,0 +1,30 @@
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
+MAX_MISTAKES = len(STAGES)
